9bd8077cbc1d805b662df91ac6f3ff542d776d64
gretagh93 <48381971+gretagh93@users.noreply.github.com>
2023-11-17 14:07:41 +0100

    firebase tests with data

--- Code/frontend.py
+++ Code/frontend.py
@@ -27,11 +27,15 @@ st.header('Hello 🌎!')
 if st.button('Balloons?'):
     st.balloons()
 
+# PLAYING WITH FIREBASE
+
+# Observe Data
+
 # Authenticate to Firestore with the JSON account key.
-db = firestore.Client.from_service_account_json("../keys/firestore-key-app-tasks.json")
+db = firestore.Client.from_service_account_json("./keys/firestore-key-app-tasks.json")
 
 # Create a reference to the Google post.
-doc_ref = db.collection("character_types").document("Broker")
+doc_ref = db.collection("character_type").document("Broker")
 
 # Then get the data at that reference.
 doc = doc_ref.get()
@@ -39,3 +43,14 @@ doc = doc_ref.get()
 # Let's see what we got!
 st.write("The id is: ", doc.id)
 st.write("The contents are: ", doc.to_dict())
+
+# Create New Data
+
+# Create a new post reference for Apple
+doc_ref = db.collection("character_type").document("Organizacion")
+
+# And then uploading some data to that reference
+doc_ref.set({
+  "code": "organizacion",
+  "funcion": "organizar"
+})
