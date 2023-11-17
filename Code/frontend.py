a7cd0dde7f3bfe6968fb13516b787d8b3abfba2f
gretagh93 <48381971+gretagh93@users.noreply.github.com>
2023-11-17 13:03:23 +0100

    Firestore configuration

--- Code/frontend.py
+++ Code/frontend.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from google.cloud import firestore
 
 from backend import *
 
@@ -26,3 +27,15 @@ st.header('Hello 🌎!')
 if st.button('Balloons?'):
     st.balloons()
 
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("../keys/firestore-key-app-tasks.json")
+
+# Create a reference to the Google post.
+doc_ref = db.collection("character_types").document("Broker")
+
+# Then get the data at that reference.
+doc = doc_ref.get()
+
+# Let's see what we got!
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())
