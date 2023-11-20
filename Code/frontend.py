156785d0a776ef1a9569ae9f42e8a11af4d82607
gretagh93 <48381971+gretagh93@users.noreply.github.com>
2023-11-20 12:36:53 +0100

    changes in key

--- Code/frontend.py
+++ Code/frontend.py
@@ -62,6 +62,7 @@ st.map(map_data)
 
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json(st.secrets["firebase"])
+st.write(st.secrets["firebase"])
 
 # Create a reference to the Google post.
 doc_ref = db.collection("character_type").document("Broker")
